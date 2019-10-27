888a3527ca622a44afc2fdf54aac932a16d9c972
arn borres <aborres@outlook.com>
2019-10-26 23:20:38 -0700

    added config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Config Parameters:
+# Case Number: This is the case number to track your status in the USCIS site
+# Twilio SSID : Get this info from your twilio account
+# Twilio API: Get this info from your twilio account
+
+CASE_NBR = 'WACXXXXXXXXXX'
+TWILIO_SSID = XX
+TWILIO_API = XX
